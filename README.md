a93192d19ae228289e74f6987851ac226910174c
Paul Flagel <paulflagel@gmail.com>
2021-12-02 17:36:53 +0100

    First part

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # IBI TP1 : Introduction à l'apprentissage profond
 
+
+
+L'objectif de ce TP est d'implémenter l'algorithme du perceptron multi-couches vu en cours, de comprendre son fonctionnement et de prendre en main le framework PyTorch.
+
+
+
+## Dataset
+
+Nous utilisons le dataset MNIST contenant des images de taille (28*28) de chiffres manuscrits.
+
+
+
+## Partie 1 : Perceptron
+
+### Dimensions des tenseurs
+
+La taille du tenseur `data_train` est de 63000 lignes (correspondant au nombre d'exemples) et 784 colonnes (correspondant à la dimension des données d'entrée). En effet, les images de taille 28*28 ont été déroulées en un vecteur de longueur 784.
+
+
+
+Le tenseur `label_train` a une taille de 63000*10 car il y a nécéssairement autant de labels que d'exemples d'entraînement. Le 10 provient de la dimension de la sortie qui est un vecteur de taille 10, contenant la probabilité que la sortie soit égale à l'un des 10 chiffres.
+
+
+
+La taille du tenseur `w` est de 784*10 puisque nous transformons nos données de taille 784 en une sortie égale à un vecteur de 10 classes (encodé en one-hot).
+
+
+
+Le tenseur de biais  `b` est de taille 1*10 car il doit être de même taille que `xw` avec `x` un exemple tiré de `data_train` (matrice d'exemples d'apprentissage).
+
+
+
+### Impact des hyperparamètres sur les performances
+
+Nous avons fait varier `eta` de 10^-2 à 10^-10 afin d'observer l'impact sur les performances. Nous avons obtenu les courbes suivantes :
+
+
+
+<img src="./images/AccuracyVSepoch.png" height="400" />
+
+Cette figure montre bien qu'un taux d'apprentissage trop grand ne permet pas la convergence (cf `1e-2` où les mises à jour des poids sont trop grandes ce qui est assimilable à un modèle aléatoire) mais qu'un taux trop faible ne permet pas d'actualiser les poids lors de la descente de gradient (à partir de `1e-7` où nous observons une nette diminution des performances, et où pour des taux encore plus faibles, le modèle n'apprend simplement pas).
+
+
+
+<img src="./images/AccuracyVSLR.png" height="400" />
+
+Nous avons tracé ici la performance maximale atteinte en fonction du taux d'apprentissage en échelle log. Nous constatons que la valeur optimale du taux d'aprentissage se situe dans notre cas entre `1e-5` et `1e-3`.
+
+
+
+
+
+## Partie 2 : Shallow Network
+
+
+
+
+
+## Partie 3 : Deep Network
+
+
+
+
+
+## Partie 4 : Pour aller plus loin (optionnel)
+
+
+
+
+
+## Ressources
+
+[Fonctions PyTorch]( https://pytorch.org/docs/stable/index.html)
+
+[Tutoriel 1]( http://pytorch.org/tutorials/beginner/pytorch_with_examples.html)
+
+[Exemples introductifs]( http://pytorch.org/tutorials/beginner/deep_learning_ 60min_blitz.html)
+
--- perceptron_pytorch.py
+++ perceptron_pytorch.py
@@ -8,51 +8,85 @@
 # ------------------------------------------------------------------------
 
 import gzip, numpy, torch
-    
-if __name__ == '__main__':
-	batch_size = 5 # nombre de données lues à chaque fois
-	nb_epochs = 10 # nombre de fois que la base de données sera lue
-	eta = 0.00001 # taux d'apprentissage
-	
-	# on lit les données
-	((data_train,label_train),(data_test,label_test)) = torch.load(gzip.open('mnist.pkl.gz'))
+import matplotlib.pyplot as plt
 
-	# on initialise le modèle et ses poids
-	w = torch.empty((data_train.shape[1],label_train.shape[1]),dtype=torch.float)
-	b = torch.empty((1,label_train.shape[1]),dtype=torch.float)
-	torch.nn.init.uniform_(w,-0.001,0.001)
-	torch.nn.init.uniform_(b,-0.001,0.001)
+if __name__ == "__main__":
+    batch_size = 5  # nombre de données lues à chaque fois
+    nb_epochs = 10  # nombre de fois que la base de données sera lue
+    eta = 0.00001  # taux d'apprentissage
 
-	nb_data_train = data_train.shape[0]
-	nb_data_test = data_test.shape[0]
-	indices = numpy.arange(nb_data_train,step=batch_size)
-	for n in range(nb_epochs):
-		# on mélange les (indices des) données
-		numpy.random.shuffle(indices)
-		# on lit toutes les données d'apprentissage
-		for i in indices:
-			# on récupère les entrées
-			x = data_train[i:i+batch_size]
-			# on calcule la sortie du modèle
-			y = torch.mm(x,w)+b
-			# on regarde les vrais labels
-			t = label_train[i:i+batch_size]
-			# on met à jour les poids
-			grad = (t-y)
-			w += eta * torch.mm(x.T,grad)
-			b += eta * grad.sum(axis=0)
+    # on lit les données
+    ((data_train, label_train), (data_test, label_test)) = torch.load(
+        gzip.open("mnist.pkl.gz")
+    )
 
-		# test du modèle (on évalue la progression pendant l'apprentissage)
-		acc = 0.
-		# on lit toutes les donnéees de test
-		for i in range(nb_data_test):
-			# on récupère l'entrée
-			x = data_test[i:i+1]
-			# on calcule la sortie du modèle
-			y = torch.mm(x,w)+b
-			# on regarde le vrai label
-			t = label_test[i:i+1]
-			# on regarde si la sortie est correcte
-			acc += torch.argmax(y,1) == torch.argmax(t,1)
-		# on affiche le pourcentage de bonnes réponses
-		print(acc/nb_data_test)
+    eta_list = [1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10]
+    results = []
+
+    for index, eta in enumerate(eta_list):
+        print(f"eta : {eta}")
+        results.append([])
+
+        # on initialise le modèle et ses poids
+        w = torch.empty((data_train.shape[1], label_train.shape[1]), dtype=torch.float)
+        b = torch.empty((1, label_train.shape[1]), dtype=torch.float)
+        torch.nn.init.uniform_(w, -0.001, 0.001)
+        torch.nn.init.uniform_(b, -0.001, 0.001)
+
+        nb_data_train = data_train.shape[0]
+        nb_data_test = data_test.shape[0]
+        indices = numpy.arange(nb_data_train, step=batch_size)
+        for n in range(nb_epochs):
+            # on mélange les (indices des) données
+            numpy.random.shuffle(indices)
+            # on lit toutes les données d'apprentissage
+            for i in indices:
+                # on récupère les entrées
+                x = data_train[i : i + batch_size]
+                # on calcule la sortie du modèle
+                y = torch.mm(x, w) + b
+                # on regarde les vrais labels
+                t = label_train[i : i + batch_size]
+                # on met à jour les poids
+                grad = t - y
+                w += eta * torch.mm(x.T, grad)
+                b += eta * grad.sum(axis=0)
+
+            # test du modèle (on évalue la progression pendant l'apprentissage)
+            acc = 0.0
+            # on lit toutes les donnéees de test
+            for i in range(nb_data_test):
+                # on récupère l'entrée
+                x = data_test[i : i + 1]
+                # on calcule la sortie du modèle
+                y = torch.mm(x, w) + b
+                # on regarde le vrai label
+                t = label_test[i : i + 1]
+                # on regarde si la sortie est correcte
+                acc += torch.argmax(y, 1) == torch.argmax(t, 1)
+            # on affiche le pourcentage de bonnes réponses
+            # print(acc / nb_data_test)
+            acc = acc.numpy()
+            acc = acc[0]
+            results[index].append(acc / nb_data_test)
+
+    x = range(1, nb_epochs + 1)
+    plt.figure()
+    for i, eta in enumerate(eta_list):
+        accuracy = results[i]
+        plt.plot(x, accuracy, label=eta)
+    plt.xlabel("Number of epochs")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy VS Number of epochs")
+    plt.legend(loc="lower right")
+    plt.tight_layout()
+    plt.xlim((1, 10))
+    plt.show()
+
+    plt.figure()
+    plt.semilogx(eta_list, [max(accuracy) for accuracy in results], "o-")
+    plt.xlabel("Learning rate")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy VS learning rate")
+    plt.tight_layout()
+    plt.show()
